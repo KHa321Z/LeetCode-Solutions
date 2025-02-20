a0d785d7395536c9937eb0682a558afe4f0d8c3f
Hammad Zaeem <70287776+KHa321Z@users.noreply.github.com>
2025-02-21 02:53:35 +0500

    Time: 20 ms (36.96%) | Memory: 25.8 MB (20.91%) - LeetSync

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
-    void recursion(vector<string>& unique, char *hapi, int n, string curr, int index = 1) {
+    void recursion(vector<string>& unique, int n, string curr, int index = 1) {
 
         if (index == n) {
             unique.push_back(curr);
             return;
         }
 
-        int ch = curr[curr.length() - 1] - 'a';
-        recursion(unique, hapi, n, curr + hapi[(ch + 1) % 3], index + 1);
-        recursion(unique, hapi, n, curr + hapi[(ch + 2) % 3], index + 1);
+        for (char ch = 'a'; ch <= 'c'; ch++)
+            if (curr[curr.length() - 1] != ch)
+                recursion(unique, n, curr + ch, index + 1);
 
     }
 
     string getHappyString(int n, int k) {
         
-        char hapi[] = {'a', 'b', 'c'};
         vector<string> unique;
 
-        recursion(unique, hapi, n, "a");
-        recursion(unique, hapi, n, "b");
-        recursion(unique, hapi, n, "c");
+        recursion(unique, n, "a");
+        recursion(unique, n, "b");
+        recursion(unique, n, "c");
 
-        sort(unique.begin(), unique.end());
         return (k <= unique.size()) ? unique[k - 1] : "";
 
     }
