258d99a1d752562d89acf7e78434d070e331371d
Hammad Zaeem <70287776+KHa321Z@users.noreply.github.com>
2026-02-28 18:56:37 +0500

    Time: 27 ms (75.17%) | Memory: 7.9 MB (89.26%) - LeetSync

--- 1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.cpp
+++ 1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.cpp
@@ -4,13 +4,8 @@ public:
         
         long long res = 0;
 
-        for (int i = 1; i <= n; i++) {
-
-            res <<= (32 - __builtin_clz(i));
-            res ^= i;
-            res %= (long long)(1e9 + 7);
-
-        }
+        for (int i = 1; i <= n; i++)
+            res = ((res << (32 - __builtin_clz(i))) ^ i) % (long long)(1e9 + 7);
 
         return res;
 
