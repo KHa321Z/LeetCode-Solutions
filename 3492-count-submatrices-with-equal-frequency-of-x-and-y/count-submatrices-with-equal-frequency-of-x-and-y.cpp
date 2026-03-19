02f649584de0c06c45daa0376a73451a8b5c10e5
Hammad Zaeem <70287776+KHa321Z@users.noreply.github.com>
2026-03-19 16:24:56 +0500

    Time: 84 ms (19.51%) | Memory: 129.3 MB (72.36%) - LeetSync

--- 3492-count-submatrices-with-equal-frequency-of-x-and-y/count-submatrices-with-equal-frequency-of-x-and-y.cpp
+++ 3492-count-submatrices-with-equal-frequency-of-x-and-y/count-submatrices-with-equal-frequency-of-x-and-y.cpp
@@ -2,27 +2,27 @@ class Solution {
 public:
     int numberOfSubmatrices(vector<vector<char>>& grid) {
         
+        bool flag = false;
         int cnt = 0, n = grid.size(), m = grid[0].size();
-        vector<vector<int>> cx(n, vector<int>(m)), cy(n, vector<int>(m));
+        vector<vector<pair<int, int>>> c(n, vector<pair<int, int>>(m));
 
         for (int i = 0; i < n; i++)
             for (int j = 0; j < m; j++) {
-                if (grid[i][j] == 'X') cx[i][j]++;
-                else if (grid[i][j] == 'Y') cy[i][j]++;
+                if (grid[i][j] == 'X') c[i][j].first++, c[i][j].second = 1;
+                else if (grid[i][j] == 'Y') c[i][j].first--, c[i][j].second = 1;
 
                 if (i > 0)
-                    cx[i][j] += cx[i - 1][j], 
-                    cy[i][j] += cy[i - 1][j];
+                    c[i][j].first += c[i - 1][j].first, 
+                    c[i][j].second |= c[i - 1][j].second;
                 
                 if (j > 0)
-                    cx[i][j] += cx[i][j - 1], 
-                    cy[i][j] += cy[i][j - 1];
+                    c[i][j].first += c[i][j - 1].first, 
+                    c[i][j].second |= c[i][j - 1].second;
 
                 if (i > 0 && j > 0)
-                    cx[i][j] -= cx[i - 1][j - 1], 
-                    cy[i][j] -= cy[i - 1][j - 1];
+                    c[i][j].first -= c[i - 1][j - 1].first;
 
-                if (cx[i][j] != 0 && cx[i][j] == cy[i][j]) cnt++;
+                if (c[i][j].second && !c[i][j].first) cnt++;
             }
 
         return cnt;
